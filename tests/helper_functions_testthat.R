a634f6b2eb295af5a337666961a2482a80c3b9cd
Marko Lalovic <marko@markolalovic.com>
2024-03-27 16:00:25 +0100

    test coverage

---
+++ tests/helper_functions_testthat.R
@@ -0,0 +1,46 @@
+library("testthat")
+library("responsesR")
+context("Testing helper functions")
+
+test_that("prop_table gives the correct result", {
+  data <- rep(c(1,2,3,4), each=2)
+  prob_tab <- get_prop_table(data, K=4)
+  correct_prob_tab <- rep(0.25, 4)
+  names(correct_prob_tab) <- 1:4
+  expect_that( identical(prob_tab, correct_prob_tab), equals(TRUE) )
+})
+
+test_that("pad_levels gives the correct result", {
+  pk <- rep(0.25, 4)
+  names(pk) <- 1:4
+  padded_pk <- pad_levels(pk = pk, K = 5)
+
+  correct_pk <- c(rep(0.25, 4), 0)
+  names(correct_pk) <- 1:5
+  expect_that( identical(padded_pk, correct_pk), equals(TRUE) )
+})
+
+test_that("percentify gives the correct result", {
+  xbreaks <- seq(from = 0, to = 1, length.out = 6)
+  xlabs <- sapply(xbreaks, percentify)
+  correct_xlabs <- c("0%", "20%", "40%", "60%", "80%", "100%")
+  expect_that( identical(xlabs, correct_xlabs), equals(TRUE) )
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
